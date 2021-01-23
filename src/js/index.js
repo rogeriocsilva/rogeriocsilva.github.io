3f0288b1c6d8d44d3f2dce9eba5480436d91eafe
Rogério Silva <rogeriosilva@Rogerios-MBP.lan>
2021-01-23 19:47:29 +0000

    feat(dark): added dark mode switch

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,26 @@
 $(function () {
-	console.log("ready!");
 	replaceImgToSvg()
+
+	const toggleButton = ".theme--switch"
+
+	var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+	if (storedTheme) {
+		document.documentElement.setAttribute('data-theme', storedTheme)
+		if (storedTheme === "dark") {
+			$(toggleButton).prop("checked", true);
+		}
+	}
+
+	$(toggleButton).click(e => {
+		var currentTheme = document.documentElement.getAttribute("data-theme");
+		var targetTheme = "light";
+		if (currentTheme === "light") {
+			targetTheme = "dark";
+		}
+
+		document.documentElement.setAttribute('data-theme', targetTheme)
+		localStorage.setItem('theme', targetTheme);
+	})
 });
 
 const replaceImgToSvg = () => {
